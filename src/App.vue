e6d6463088f4379eb01a5ab8870463d99b428545
Mohammadreza Varasteh <varastehmr@gmail.com>
2020-11-22 17:01:55 +0330

    UPDATE change Gallery to a componet

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,21 @@
 <template>
   <v-app>
     <Title />
-    <v-main>
-      <v-container>
-        <v-card flat>
-          <v-layout row>
-            <div>
-              <v-flex xs6 md3>
-                <v-responsive></v-responsive>
-              </v-flex>
-              <v-flex xs6 md3>
-                <v-card-text></v-card-text>
-              </v-flex>
-            </div>
-          </v-layout>
-        </v-card>
-      </v-container>
-    </v-main>
+    <Gallery />
   </v-app>
 </template>
 
 <script>
 import Title from "./components/Title";
+import Gallery from "./components/Gallery";
+import Gallery from "./components/Gallery.vue";
 
 export default {
   name: "App",
 
   components: {
     Title,
+    Gallery,
   },
 
   data: () => ({
---
+++ src/components/Gallery.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-main>
+    <v-container>
+      <v-card flat>
+        <v-layout row>
+          <div>
+            <v-flex xs6 md3>
+              <v-responsive></v-responsive>
+            </v-flex>
+            <v-flex xs6 md3>
+              <v-card-text></v-card-text>
+            </v-flex>
+          </div>
+        </v-layout>
+      </v-card>
+    </v-container>
+  </v-main>
+</template>
+<script>
+export default {};
+</script>
