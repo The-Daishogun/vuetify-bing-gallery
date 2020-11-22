4e48c1a6969f17db4ced5cb766e99fcea20efd5e
Mohammadreza Varasteh <varastehmr@gmail.com>
2020-11-22 16:51:02 +0330

    ADD title bar basic index page skeleten

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,33 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <Title />
     <v-main>
-      <HelloWorld/>
+      <v-container>
+        <v-card flat>
+          <v-layout row>
+            <div>
+              <v-flex xs6 md3>
+                <v-responsive></v-responsive>
+              </v-flex>
+              <v-flex xs6 md3>
+                <v-card-text></v-card-text>
+              </v-flex>
+            </div>
+          </v-layout>
+        </v-card>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Title from "./components/Title";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    Title,
   },
 
   data: () => ({
---
+++ src/components/Title.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app-bar app color="primary" flat>
+    <v-app-bar-title class="text-uppercase white--text">
+      Bing Image Gallery
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Title",
+};
+</script>
