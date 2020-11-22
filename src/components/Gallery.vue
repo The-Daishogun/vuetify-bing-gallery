25a6b2a02b6f18297093e5ea7be47ad35f19d653
Mohammadreza Varasteh <varastehmr@gmail.com>
2020-11-22 17:48:35 +0330

    UPDATES gets data from api and adds them to page

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -3,12 +3,19 @@
     <v-container>
       <v-card flat>
         <v-layout row>
-          <div>
+          <div v-for="img in imgs" :key="img.id">
             <v-flex xs6 md3>
-              <v-responsive></v-responsive>
+              <v-responsive>
+                <img :src="`${img.url}`" alt="" />
+              </v-responsive>
             </v-flex>
             <v-flex xs6 md3>
-              <v-card-text></v-card-text>
+              <v-card-text class="float-right">
+                <h1 class="subtitle-1">{{ img.title }}</h1>
+                <p>
+                  {{ img.copyright }}
+                </p>
+              </v-card-text>
             </v-flex>
           </div>
         </v-layout>
@@ -17,5 +24,29 @@
   </v-main>
 </template>
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      imgs: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("http://localhost:5000/api/7")
+      .then((response) => {
+        const arr = [];
+        Object.keys(response.data).forEach((key) =>
+          arr.push({
+            id: key,
+            copyright: response.data[key].copyright,
+            url: response.data[key].url,
+            title: response.data[key].title,
+          })
+        );
+        this.imgs = arr;
+      })
+      .catch((err) => console.log(err));
+  },
+};
 </script>
