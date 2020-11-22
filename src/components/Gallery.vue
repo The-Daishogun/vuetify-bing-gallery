68e2c1c71db200f36703897eb9badcfd96f1ac48
Mohammadreza Varasteh <varastehmr@gmail.com>
2020-11-22 23:55:28 +0330

    ADD image gallery

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,27 +1,24 @@
 <template>
-  <v-main>
-    <v-container>
-      <v-card flat>
+  <v-layout row class="justify-space-around">
+    <v-flex xs12 md3 v-for="img in imgs" :key="img.id" class="ma-3">
+      <v-card class="pa-3">
         <v-layout row>
-          <div v-for="img in imgs" :key="img.id">
-            <v-flex xs6 md3>
-              <v-responsive>
-                <img :src="`${img.url}`" alt="" />
-              </v-responsive>
-            </v-flex>
-            <v-flex xs6 md3>
-              <v-card-text class="float-right">
-                <h1 class="subtitle-1">{{ img.title }}</h1>
-                <p>
-                  {{ img.copyright }}
-                </p>
-              </v-card-text>
-            </v-flex>
-          </div>
+          <v-flex xs12 md5>
+            <v-img :src="`${img.url}`" class="mx-3"></v-img>
+          </v-flex>
+          <v-flex xs12 md7>
+            <h1 class="subtitle-1 mx-3">{{ img.title }}</h1>
+            <p class="caption text-truncate mx-3" :id="`text-${img.id}`">
+              {{ img.copyright }}
+            </p>
+            <v-btn small text :id="`${img.id}`" @click="showText"
+              >Show Text</v-btn
+            >
+          </v-flex>
         </v-layout>
       </v-card>
-    </v-container>
-  </v-main>
+    </v-flex>
+  </v-layout>
 </template>
 <script>
 import axios from "axios";
@@ -30,6 +27,12 @@ export default {
     return {
       imgs: [],
     };
+  },
+  methods: {
+    showText(event) {
+      var id = event.target.parentElement.id;
+      document.getElementById(`text-${id}`).classList.toggle("text-truncate");
+    },
   },
   mounted() {
     axios
