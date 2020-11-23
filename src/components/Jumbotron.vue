bee88b2aee2aaee129c44194fe211b48fac41309
Mohammadreza Varasteh <varastehmr@gmail.com>
2020-11-23 09:35:05 +0330

    ADD Jumbotron.vue

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-layout row class="justify-center my-5">
+    <v-flex md9>
+      <v-card class="justify-space-around" shaped height="20%">
+        <v-img src="../assets/test.jpg"></v-img>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+export default {};
+</script>
