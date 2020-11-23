046751e501013d768a910ad94987cf293905696c
Mohammadreza Varasteh <varastehmr@gmail.com>
2020-11-23 11:27:23 +0330

    ADD uses props to change the big picture in jumbotron

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -2,11 +2,13 @@
   <v-layout row class="justify-center my-5">
     <v-flex md9>
       <v-card class="justify-space-around" shaped height="20%">
-        <v-img src="../assets/test.jpg"></v-img>
+        <v-img :src="`${url}`"></v-img>
       </v-card>
     </v-flex>
   </v-layout>
 </template>
 <script>
-export default {};
+export default {
+  props: ["url"],
+};
 </script>
