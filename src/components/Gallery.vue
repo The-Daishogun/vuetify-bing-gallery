f29ffb8bc0aa87afccbab4fb15228c888d4163d8
Mohammadreza Varasteh <varastehmr@gmail.com>
2020-11-23 09:02:59 +0330

    UPDATE streamlined the data handling

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,19 +1,17 @@
 <template>
   <v-layout row class="justify-space-around">
-    <v-flex xs12 md3 v-for="img in imgs" :key="img.id" class="ma-3">
+    <v-flex xs12 md3 v-for="(value, key) in imgs" :key="key" class="ma-3">
       <v-card class="pa-3">
         <v-layout row>
           <v-flex xs12 md5>
-            <v-img :src="`${img.url}`" class="mx-3"></v-img>
+            <v-img :src="`${value.url}`" class="mx-3"></v-img>
           </v-flex>
           <v-flex xs12 md7>
-            <h1 class="subtitle-1 mx-3">{{ img.title }}</h1>
-            <p class="caption text-truncate mx-3" :id="`text-${img.id}`">
-              {{ img.copyright }}
+            <h1 class="subtitle-1 mx-3">{{ value.title }}</h1>
+            <p class="caption text-truncate mx-3" :id="`text-${key}`">
+              {{ value.copyright }}
             </p>
-            <v-btn small text :id="`${img.id}`" @click="showText"
-              >Show Text</v-btn
-            >
+            <v-btn small text :id="`${key}`" @click="showText">Show Text</v-btn>
           </v-flex>
         </v-layout>
       </v-card>
@@ -38,16 +36,7 @@ export default {
     axios
       .get("http://localhost:5000/api/7")
       .then((response) => {
-        const arr = [];
-        Object.keys(response.data).forEach((key) =>
-          arr.push({
-            id: key,
-            copyright: response.data[key].copyright,
-            url: response.data[key].url,
-            title: response.data[key].title,
-          })
-        );
-        this.imgs = arr;
+        this.imgs = response.data;
       })
       .catch((err) => console.log(err));
   },
